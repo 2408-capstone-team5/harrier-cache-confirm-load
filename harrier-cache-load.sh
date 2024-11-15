a3b973eda67afcd2e21e3e06e689f0fba243df1b
Jesse Kercheval <56614846+jessekerch@users.noreply.github.com>
2024-11-15 09:24:57 -0800

    Update harrier-cache-load.sh

--- harrier-cache-load.sh
+++ harrier-cache-load.sh
@@ -5,7 +5,7 @@ if [ -f "../../../s3bucket/node_modules_cached_tar/${GITHUB_REPOSITORY_OWNER}-${
 then
   echo "cached tar file exists."
   cp ../../../s3bucket/node_modules_cached_tar/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz ./
-  tar -xvzf ${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz
+  tar -xzf ${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz
 else
   echo "cached tar file does not exist."
 fi
