f6f52f463ced68b28943fae566789f97f7909d3f
Jesse Kercheval <56614846+jessekerch@users.noreply.github.com>
2024-11-15 08:56:38 -0800

    Update harrier-cache-load.sh

--- harrier-cache-load.sh
+++ harrier-cache-load.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Check for existing node_modules_cached_tar in s3bucket
-if [ -f "../../../s3bucket/node_modules_cached_tar/${{ github.repository_owner }}-${{ github.event.repository.name }}-${GITHUB_REF#refs/heads/}.tar.gz" ]
+if [ -f "../../../s3bucket/node_modules_cached_tar/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz" ]
 then
   echo "cached tar file exists."
-  cp ../../../s3bucket/node_modules_cached_tar/${{ github.repository_owner }}-${{ github.event.repository.name }}-${GITHUB_REF#refs/heads/}.tar.gz ./
-  tar -xvzf ${{ github.repository_owner }}-${{ github.event.repository.name }}-${GITHUB_REF#refs/heads/}.tar.gz
+  cp ../../../s3bucket/node_modules_cached_tar/${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz ./
+  tar -xvzf ${GITHUB_REPOSITORY_OWNER}-${GITHUB_REPOSITORY##*/}-${GITHUB_REF#refs/heads/}.tar.gz
 else
   echo "cached tar file does not exist."
 fi
